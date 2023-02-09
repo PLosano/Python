ad8f9ff8cc99e67a79d4374a55f2fc9ef172a594
PLosano <paulalosano13@gmail.com>
2023-02-09 18:41:09 -0300

    If and operators

--- Python_1.py
+++ Python_1.py
@@ -151,3 +151,95 @@ a = 0
 print(a)
 print(b)
 print(c)
+
+#Lists 
+# Declare a List to Store Your Items
+
+#Lists are objects that can contain a collection of objects of any type.  We can have a list containing several integers (1, 2, 50, 2,000 or more, it doesn't matter), a list containing floats, a list containing strings, or even a list mixing objects of different types.
+#Lists are ordered objects - This number is called an index and it starts at 0 (not 1!). 
+
+customerName = ['Marion Weaver', 'Alberto Mendoza', 'Katharine Tyler', 'Isaac Steele']
+
+# assign the value 'Marianne Weaver' to the first name in our list
+# it is index 0, because indices start at 0 in python!
+customerName[0] = 'Marianne Weaver'
+print(customerName[0])
+
+
+#Python also lets you use negative indices to access or modify an item. The index -1 corresponds to the last item of the list, -2 to the second last, and so on. You can also access an index range by using the  :  operator. For example, 1:3 will let you access items two to four.
+# print the last item
+print(customerName[-1])
+
+# access the second item to the 3rd
+print(customerName[1:3])
+
+# access all items from the beginning to the second
+print(customerName[:2])
+
+strangeList = [4, 10.2, 'Marion Weaver', ['another list', 1]]
+
+# print the 4th item of the list
+print(strangeList[3])
+
+#Thanks to the different list methods, we can:
+
+#search for a specific item in the list.
+#add a new item at the end.
+#insert a new item at a specific index.
+#delete an item from the list.
+
+list = []
+list.append(7)
+list.append(5)
+print(list) # => [7, 5]
+
+list = []
+list.append(7) # -> [7]
+list.append(5) # -> [7, 5]
+list.insert(1,12) # [7, 12, 5]
+list[0] = 4 # -> [4, 12, 5]
+list.remove(12) # [4, 5]
+list.index(5) # prints 1
+list.extend([1, 2, 3]) # [4, 5, 1, 2, 3]
+del list[3] # [4, 5, 1, 3]
+
+# The   len()  function lets you retrieve the size of your list:
+list = [1, 2, 3]
+len(list) # will print 3
+
+#Dictionaries
+# Dictionaries are another type of object, similar to lists, but which will let you do this with a single variable! Indeed, a dictionary is a list of items organized via a system of keys. With a real dictionary, you look up a word to access its definition. In programming, this word corresponds to the key and the definition to the value associated with it. This is called a key-value pair. 
+# Each key in a dictionary must be unique. Strings are generally used to define keys, but this is not a requirement, per se.
+
+accounts = {'Marion Weaver': 10000, 'Alberto Mendoza': 150, 'Katharine Tyler': 300, 'Isaac Steele': 1800.74}
+print(accounts['Alberto Mendoza']) # -> 150
+
+#Here are the operations frequently carried out with dictionaries:
+#Access the value of an item
+#Add a new item (a new key-value pair)
+#Delete an item via its key
+
+accounts['Marion Weaver'] -= 2000 # I subtract 2000 from David's account
+accounts['Kristian Roach'] = 1000 # I add a new individual in my dictionary
+print(accounts['Kristian Roach']) # I print the value of Kristian's account
+
+accounts.pop('Alberto Mendoza') # deletes Alberto Mendoza from our dictionary
+
+#Len can be use to 
+len(accounts) # -> 3
+
+#Tuples
+
+#The last type of collection we will look at are tuples. These are very similar to lists:
+#They are ordered objects, so we can access the different items stored in a tuple from their index.
+#You can store any kind of object in a tuple.
+#The main difference is that once a tuple has been declared, it cannot be modified. It is then said that it is immutable.
+
+my_tuple = (1, 2, 3, 'a', 'b')
+
+print(my_tuple[1]) # -> 2
+print(my_tuple[4]) # -> 'b'
+
+a, b = (1, 'apple')
+print(a) # -> 1
+print(b) # -> 'apple'
